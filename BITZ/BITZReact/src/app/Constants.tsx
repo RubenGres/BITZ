2cd1d301e93ef8cac6bfadfab5999dc8682642b6
RubenGres <ruben.gres@proton.me>
2025-05-31 23:08:00 +0000

    Use NEXT_PUBLIC_API_URL to  fetch the API URL

--- BITZ/BITZReact/src/app/Constants.tsx
+++ BITZ/BITZReact/src/app/Constants.tsx
@@ -1,2 +1,1 @@
-// export const API_URL = "https://scaling-space-carnival-qvvrrjxqgrp246pj-5000.app.github.dev"
-export const API_URL = "https://api.bitz.tools"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.bitz.tools"
